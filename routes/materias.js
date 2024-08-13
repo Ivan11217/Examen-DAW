b96920d5c889adbf23af9a73df22cc498b81db6d
Ivan <ivannunez@uees.edu.ec>
2024-08-13 08:46:52 -0500

    a

--- routes/materias.js
+++ routes/materias.js
@@ -16,7 +16,14 @@ router.get('/crear', async (req, res) => {
 // Vista para listar materias
 router.get('/', async (req, res) => {
   try {
-    const materias = await Materia.findAll({ include: Area });
+    const materias = await Materia.findAll({
+      include: [
+        {
+          model: Area,
+          as: 'area'
+        }
+      ]
+    });
     res.render('listar_materias', { materias });
   } catch (error) {
     console.error(error);
